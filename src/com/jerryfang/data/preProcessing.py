eac0238eeb7c30dcf0750d4e795137d0e66cfe4f
XuliangFang <jerryfang1995@foxmail.com>
2017-04-10 20:54:49 +0800

    process data set

--- src/com/jerryfang/data/preProcessing.py
+++ src/com/jerryfang/data/preProcessing.py
@@ -1,2 +1,22 @@
+# -*- coding: UTF-8 -*-
+# pre processing data set
+# function: filter records(up & down)
+# usage: python preProcessing.py filepath
 import os
-import os.string
+import sys
+import string
+
+def processData(arg1, arg2):
+    print "The parameter you input "+arg1 + " " + arg2
+    rawFile = arg1
+    newFile = open(arg2, 'w')
+    with open(rawFile) as file:
+        for line in file:
+            if line.find("up") != -1 && line.find("down") != -1:
+                newFile.write(line)
+    file.close()
+    newFile.close()
+    return
+
+if __name__=='__main__':
+    processData('/home/hadoop/trojanData/result.txt', '/home/hadoop/trojanData/dataSet.txt')
