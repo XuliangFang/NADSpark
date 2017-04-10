0b1d3280c12b3d82336e1f346d1d29ae41af5e41
XuliangFang <jerryfang1995@foxmail.com>
2017-04-10 21:40:47 +0800

    update date set process.py

--- src/com/jerryfang/data/preProcessing.py
+++ src/com/jerryfang/data/preProcessing.py
@@ -12,8 +12,37 @@ def processData(arg1, arg2):
     newFile = open(arg2, 'w')
     with open(rawFile) as file:
         for line in file:
-            if(line.find("up") != -1 and line.find("down") != -1):
-                newFile.write(line)
+            if (line.find("up") != -1 and line.find("down") != -1):
+                #newFile.write(line)
+                # DNS behavior
+                dnsNum = 0
+                dnsIndex = line.find("dns")
+                if(dnsIndex!= -1):
+                    print "Expected DNS number: " + line[dnsIndex+1: line.find(' ', dnsIndex, len(line))]
+                else
+                    print "Expected DNS number: 0"
+
+                #Ratio of upload package number/download package number
+                upIndex = line.find("up")
+                print "upload package number: " + line[upIndex: line.find(' ', upIndex, len(line))]
+                downIndex = line.find("down")
+                print "upload package number: " + line[downIndex: line.find(' ', downIndex, len(line))]
+                ratio = 0.0
+
+                #Ratio of upload package size/download package size
+                upsizeIndex = line.find(' ', upIndex, len(line))+1
+                print "upload package size: " + line[upsizeIndex : line.find(' ', upsizeIndex, len(line))]
+                downsizeIndex = line.find(' ', downIndex, len(line))+1
+                print "download package size: " + line[downsizeIndex : line.find(' ', downsizeIndex, len(line))]
+
+                #Proportion of SYN packages
+                
+
+                #Proportion of PSH packages
+
+                #Proportion of small packages
+
+
     file.close()
     newFile.close()
     return
